164b210ee9c276ba749708ed7552ffab7a5cf6e7
omobosteven <omobosteven@gmail.com>
2022-07-20 13:26:50 +0100

    create user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from 'src/users/users.module';
+import { ReportsModule } from 'src/reports/reports.module';
+import { User } from 'src/users/user.entity';
+import { Report } from 'src/reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
